32cc42ce2f6ab5bae5fa2acf85c01846acfa62e6
ytake <yuuki.takezawa@comnect.jp.net>
2014-01-28 19:02:17 +0900

    en route
    added support mail(swift mailer)
    en route(logger etc.)

---
+++ src/Comnect/Support/Logger.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Logger.php
+ * @author yuuki.takezawa<yuuki.takezawa@comnect.jp.net>
+ */
+namespace Comnect\Support;
+
+
+class Logger {
+
+}
---
+++ src/Comnect/Support/Mail.php
@@ -0,0 +1,124 @@
+<?php
+namespace Comnect\Support;
+/**
+ * Mail.php
+ * @author yuuki.takezawa<yuuki.takezawa@comnect.jp.net>
+ */
+
+/**
+ * Class Mail
+ * @package Comnect\Support
+ */
+class Mail {
+
+	/** @var \Swift_Mailer */
+	protected $mailer;
+	/** @var  \Comnect\Support\Config */
+	protected $config;
+
+	public function __construct(Config $config)
+	{
+		$this->message = \Swift_Message::newInstance();
+		$this->config = $config;
+	}
+	
+	/**
+	 * get mail
+	 */
+	public function getMailer()
+	{
+		return $this;
+	}
+	
+	/**
+	 * set subject
+	 * @param string $subject
+ 	 * @return \Swift_Mime_SimpleMessage
+	 */
+	public function subject($subject)
+	{
+		$this->message->setSubject($subject);
+		return $this;
+	}
+
+	/**
+	 * set from addresses
+	 * @param array $addresses
+	 * @param null|string $name
+	 * @return \Swift_Mime_SimpleMessage
+	 */
+	public function from(array $addresses, $name = null)
+	{
+		$this->message->setFrom($addresses, $name);
+		return $this;
+	}
+
+	/**
+	 * set to
+	 * @param array $addresses
+	 * @param null|string $name
+	 * @return \Swift_Mime_SimpleMessage
+	 */
+	public function to(array $addresses, $name = null)
+	{
+		$this->message->setTo($addresses, $name);
+		return $this;
+	}
+
+	/**
+	 * set body
+	 * @param string $body
+	 * @param null $contentType
+	 * @param null $charset
+	 * @return $this
+	 */
+	public function body($body, $contentType = null, $charset = null)
+	{
+		$this->message->setBody($body, $contentType, $charset);
+		return $this;
+	}
+
+	/**
+	 * attach
+	 * @param null $data
+	 * @param null $filename
+	 * @param null $contentType
+	 * @return $this
+	 */
+	public function attach($data = null, $filename = null, $contentType = null)
+	{
+		$this->message->attach(\Swift_Attachment::newInstance($data, $filename, $contentType));
+		return $this;
+	}
+
+
+	/**
+	 * send mail
+	 * @return int
+	 */
+	public function send()
+	{
+		$mailConfigure = $this->config->get('mail');
+		switch($mailConfigure['driver'])
+		{
+			case 'sendmail':
+				$transport = \Swift_SendmailTransport::newInstance($mailConfigure['sendmail']);
+				break;
+
+			case 'smtp':
+				$transport = \Swift_SmtpTransport::newInstance(
+					$mailConfigure['host'], $mailConfigure['port'], $mailConfigure['protocol']);
+
+				if(!is_null($mailConfigure['account'])){
+					$transport->setUsername($mailConfigure['account']);
+				}
+
+				if(!is_null($mailConfigure['password'])){
+					$transport->setPassword($mailConfigure['password']);
+				}
+				break;
+		}
+		$mailer = \Swift_Mailer::newInstance($transport);
+		return $mailer->send($this->message);
+	}
+}
